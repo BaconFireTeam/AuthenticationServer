002bb4cd1d63e2ab37ed4742cbbecdbc7ad2fd86
AthleticCorgi <peterli342@gmail.com>
2020-03-24 02:16:28 -0400

    first commit

---
+++ src/test/java/com/baconfire/onboardwebapp/security/springsecurityjwt/SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baconfire.onboardwebapp.security.springsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
